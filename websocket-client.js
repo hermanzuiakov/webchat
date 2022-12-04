93faff3fea1c5335d4a038123911e504088e53dd
Herman Zuiakov <gzuyakov@gmail.com>
2022-12-04 16:52:29 +0100

    Client communication

--- websocket-client.js
+++ websocket-client.js
@@ -1,11 +1,34 @@
 const WS = new WebSocket('ws://localhost:3232');
 
 WS.onmessage = (payload)=>{
+    displayMessages(payload.data);
     console.log(payload.data);
+}
+
+WS.onopen = () => {
+    displayTitle("CONNECTED TO SERVER");
+    console.log("CONNECTION IS OPENED");
+}
+WS.onclose = () => {
+    displayTitle("UNCONNECTED TO SERVER");
+    console.log("CONNECTION IS CLOSED");
+}
+
+function displayTitle(title) {
+    document.querySelector('h1').innerHTML = title;
+}
+
+function displayMessages(message) {
+    const messagesWrapper = document.getElementById('messages');
+
+    let messageBox = document.createElement('div');
+    messageBox.classList.add('message');
+    messageBox.innerHTML = `<div class="message">${message}</div>`;
+
+    messagesWrapper.appendChild(messageBox);
 }
 
 document.forms[0].onsubmit = ()=>{
     let inputValue = document.getElementById('message').value;
-
     WS.send(inputValue);
 }
--- websocket-server.js
+++ websocket-server.js
@@ -1,14 +1,19 @@
 const WebsocketServer = require('ws'),
     WSS = new WebsocketServer.Server({port: 3232});
 
-
 WSS.on('connection', (ws)=>{
     ws.on('message', (message)=>{
-        WSS.clients.forEach((client)=>{
-            client.send(message.toString());
-        })
+        let messageText = message.toString();
 
-        console.log(message.toString());
+        if (messageText == "/close"){
+            ws.close();
+        } else {
+            WSS.clients.forEach((client)=>{
+                client.send(messageText);
+            })
+        }
+
+        console.log(messageText);
     });
 
     console.log('Connected');
